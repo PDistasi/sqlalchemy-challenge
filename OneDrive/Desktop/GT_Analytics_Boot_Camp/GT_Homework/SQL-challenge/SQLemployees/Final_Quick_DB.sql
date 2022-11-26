b6d8f7d804e08805823cf2761de18b40573ef3c2
PDistasi <112498067+PDistasi@users.noreply.github.com>
2022-11-26 12:33:46 -0500

    Added Character limits

--- OneDrive/Desktop/GT_Analytics_Boot_Camp/GT_Homework/SQL-challenge/SQLemployees/Final_Quick_DB.sql
+++ OneDrive/Desktop/GT_Analytics_Boot_Camp/GT_Homework/SQL-challenge/SQLemployees/Final_Quick_DB.sql
@@ -4,8 +4,8 @@
 
 
 CREATE TABLE "titles" (
-    "title_id" VARCHAR   NOT NULL,
-    "title" VARCHAR   NOT NULL,
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
@@ -13,11 +13,11 @@ CREATE TABLE "titles" (
 
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
-    "title_id" VARCHAR   NOT NULL,
+    "title_id" VARCHAR(5)   NOT NULL,
     "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "sex" VARCHAR   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
     "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
@@ -26,22 +26,22 @@ CREATE TABLE "employees" (
 
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
-    "salary" VARCHAR   NOT NULL,
+    "salary" INT   NOT NULL,
     CONSTRAINT "pk_salaries" PRIMARY KEY (
         "emp_no"
      )
 );
 
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(30)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
     "emp_no" INT   NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY (
         "dept_no","emp_no"
@@ -50,7 +50,7 @@ CREATE TABLE "dept_manager" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "emp_no","dept_no"
      )
