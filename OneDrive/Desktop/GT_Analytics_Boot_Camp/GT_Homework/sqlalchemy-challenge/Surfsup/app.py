101875f7823b205f22b82a37652528a9b6465424
PDistasi <112498067+PDistasi@users.noreply.github.com>
2022-12-03 12:41:19 -0500

    Saving current work

---
+++ OneDrive/Desktop/GT_Analytics_Boot_Camp/GT_Homework/sqlalchemy-challenge/Surfsup/app.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+precipitation = Base.classes.precipitation
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of Hawaii Precipitation for the most recent 12 months of data"""
+    # Query precipitation analysis
+    results = session.query(precipitation.date, precipitation.prcp).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    total_prcp = list(np.ravel(results))
+
+    return jsonify(total_prcp)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all stations in Hawaii"""
+    # Return a JSON list of stations from the dataset
+    results = session.query(stations.station).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    most_active = {}
+    for name, age, sex in results:
+        passenger_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
